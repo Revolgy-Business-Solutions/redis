cce242c49d758ff2f0a498fd025d68a028d0f372
Alex Gain <31071302+aleksgain@users.noreply.github.com>
2023-08-23 16:10:35 +0200

    Update create-tags.sh

--- create-tags.sh
+++ create-tags.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# loop over the arguments
 for tag in "$@"
 do
-  # create a tag with the format postgres-{version}
   git tag "$tag"
-  # push the tag to the remote
   git push origin "$tag"
 done
